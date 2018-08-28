4261420198d936edf6f9fc2a01caa0b2ba8d6c87
WrothSquid37 <cadeynrauscher@gmail.com>
2018-08-27 21:33:56 -0700

    Add Enemies

---
+++ Third Person Shooter/Assets/MoveToPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveToPlayer : MonoBehaviour {
+
+	public Transform player;
+
+	private NavMeshAgent agent;
+
+	void Start()
+	{
+		agent = GetComponent<NavMeshAgent>();
+	}
+
+	void Update()
+	{
+		agent.SetDestination(player.transform.position);
+	}
+
+
+
+}
